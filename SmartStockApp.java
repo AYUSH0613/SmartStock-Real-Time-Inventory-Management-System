f1e362478728848b791c9f02c61a7b5a22e9afad
AYUSH0613 <ayushparoliya2004@gmail.com>
2025-07-12 17:54:33 +0530

    Add Product, ActionType, and Action classes to manage inventory actions

--- SmartStockApp.java
+++ SmartStockApp.java
@@ -1,8 +1,37 @@
 package SmartStock;
 import java.util.*;
 
+class Product {
+    String name;
+    int quantity;
+
+    Product(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
+
+enum ActionType {
+    ADD, UPDATE, DELETE
+}
+
+class Action {
+    ActionType type;
+    String productId;
+    Product previousState;
+
+    Action(ActionType type, String productId, Product previousState) {
+        this.type = type;
+        this.productId = productId;
+        this.previousState = previousState;
+    }
+}
+
 public class SmartStockApp{
     static Scanner scan=new Scanner(System.in);
+    static HashMap<String, Product> inventory = new HashMap<>();
+    static Stack<Action> actionHistory = new Stack<>();
+
     public static void main(String[] args) {
         
     }
