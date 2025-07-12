023c9115830e3f1ba503122a79accc94f9ee53d9
AYUSH0613 <ayushparoliya2004@gmail.com>
2025-07-12 17:51:14 +0530

    initialize scanner

--- SmartStockApp.java
+++ SmartStockApp.java
@@ -2,6 +2,7 @@ package SmartStock;
 import java.util.*;
 
 public class SmartStockApp{
+    static Scanner scan=new Scanner(System.in);
     public static void main(String[] args) {
         
     }
